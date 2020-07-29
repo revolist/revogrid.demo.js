5cd20b859d83e294619708835d0121152102276a
kumakint <maksim.k@revolist.eu>
2020-07-29 16:51:28 +0300

    added sorting

--- stories/vuejs.stories.js
+++ stories/vuejs.stories.js
@@ -13,13 +13,16 @@ export const basicSample = () => {
         Vue.config.ignoredElements = [/revo-\w*/]; // Set ignore web-component and avoid parsing it as vuejs
 
         Vue.component('revogrid-vue-wrapper', {
-            template: '<revo-grid class="grid-component" :source.prop="source" :columns.prop="columns" :dimensions.prop="dimensions"/>',
+            template: '<revo-grid class="grid-component" :source.prop="source" :columns.prop="columns" :dimensions.prop="dimensions" @beforeEdit="beforeEdit" @headerClick="columnClick"/>',
             data() {
                 return {
-                    columns: [{
-                        prop: 'name',
-                        name: 'Name'
-                    },
+                    asc: true,
+                    columns: [
+                        {
+                            prop: 'name',
+                            name: 'Name',
+                            readonly: true
+                        },
                         {
                             prop: 'eyeColor',
                             name: 'Eyes',
@@ -35,17 +38,31 @@ export const basicSample = () => {
                             name: 'Gender',
                             cellTemplate: (h, props) => {
                                 const text = props.model[props.prop];
-                                return h('i', {
-                                    class: `fas ${text === 'male' ? 'fa-mars' : 'fa-venus'}`
-                                },  '');
+                                return h('i', { class: `fas ${text === 'male' ? 'fa-mars' : 'fa-venus'}` },  '');
                             }
-                        }],
+                        }
+                    ],
                     source: people,
                     dimensions: {
                         col: { 0: 500 }
                     }
                 };
-            }
+            },
+            methods: {
+                beforeEdit(e)  {
+                    // e.detail
+                },
+                columnClick(e) {
+                    const col = e.detail.prop;
+                    const s = this.source.sort((a, b) => {
+                        if(a[col] < b[col]) { return this.asc ? -1 : 1; }
+                        if(a[col] > b[col]) { return this.asc ? 1 : -1; }
+                        return 0;
+                    });
+                    this.asc = !this.asc;
+                    this.source = [...s];
+                }
+            },
         });
 
         new Vue({ render: h => h('revogrid-vue-wrapper') }).$mount('#app');
